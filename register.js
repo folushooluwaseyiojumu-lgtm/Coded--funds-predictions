4a5c5b2e29b30eb19d9429ac2fd1035c193a1b1c
Coded funds -predictions <folushooluwaseyiojumu@gmail.com>
2026-07-04 08:35:08 +0100

    added firebase registration

---
+++ register.js
@@ -0,0 +1,32 @@
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const registerBtn = document.getElementById("registerBtn");
+
+registerBtn.addEventListener("click", async () => {
+    const fullName = document.getElementById("fullname").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (!fullName || !email || !password || !confirmPassword) {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match.");
+        return;
+    }
+
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        alert("Account created successfully!");
+
+        window.location.href = "login.html";
+
+    } catch (error) {
+        alert(error.message);
+    }
+});
