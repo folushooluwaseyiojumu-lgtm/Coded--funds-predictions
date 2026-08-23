86721b750d5038596f79769421135541327fb605
Coded funds -predictions <folushooluwaseyiojumu@gmail.com>
2026-08-23 06:48:50 +0100

    Refactor login logic and update redirect URL

--- login.js
+++ login.js
@@ -1,21 +1,31 @@
 import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 const email = document.querySelector('input[type="email"]');
 const password = document.querySelector('input[type="password"]');
 const button = document.querySelector("button");
 
 button.addEventListener("click", async () => {
+
     try {
+
         await signInWithEmailAndPassword(
             auth,
-            email.value,
+            email.value.trim(),
             password.value
         );
 
         alert("Login successful!");
-        window.location.href = "VIP.html";
+
+        window.location.href = "dashboard.html";
+
     } catch (error) {
+
         alert(error.message);
+
     }
+
 });
