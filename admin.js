15f7eb2a4ba05a368c839b12fb6597d36cae4259
Coded funds -predictions <folushooluwaseyiojumu@gmail.com>
2026-08-25 01:07:26 +0100

    Remove current results section from admin.js
    Removed HTML elements for current results display.

--- admin.js
+++ admin.js
@@ -325,19 +325,7 @@ document
         }
 
     });
-<hr>
 
-<h3>
-    📋 Current Results
-</h3>
-
-<div id="resultsList">
-
-    <p>
-        Loading results...
-    </p>
-
-</div>
 
 // ==========================================
 // LOAD RESULTS
